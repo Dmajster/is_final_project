c6192063e57e9b5e889bd143b1af5a5415734a10
Domen Rostohar <domen.rostohar98@gmail.com>
2020-01-11 19:50:15 +0100

    auth

---
+++ MusicShare/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace MusicShare.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName;
+        public string LastName;
+        public string City;
+    }
+}
