e63d56dba97858cfda71184bf1b041af6e57d72b
dmajster <domen.rostohar98@gmail.com>
2020-01-12 23:57:30 +0100

    added for loop to home page

--- MusicShareWeb/Controllers/HomeController.cs
+++ MusicShareWeb/Controllers/HomeController.cs
@@ -1,21 +1,32 @@
 ﻿using System.Diagnostics;
+using System.Linq;
 using ContosoPetsWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using MusicShare.Data;
 
 namespace MusicShareWeb.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MusicShareContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MusicShareContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            ViewData["Posts"] = _context.Posts
+                .Include(p => p.Artist)
+                .Include(p => p.Song)
+                .OrderBy(p => p.ViewCount)
+                .ToList();
+
             return View();
         }
 
